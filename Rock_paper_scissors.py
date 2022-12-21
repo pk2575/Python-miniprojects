bfebb64300bacf95c0c5bf3f3c9720d74c4562c4
pawan kharel <pawankharel10@gmail.com>
2022-12-21 14:50:04 +0545

    optimization of Rock paper scissors

--- Rock_paper_scissors.py
+++ Rock_paper_scissors.py
@@ -2,30 +2,35 @@ import random
 
 
 playlist = ["r", "p", "s"]
-keys = { "r" : "rock", "p" : "paper", "s":"scissors"}
-win = {"r": "s", "p":"r", "s":"p"}
+names = { "r" : "rock", "p" : "paper", "s":"scissors"}
+keys = {"r": "s", "p":"r", "s":"p"}
 
 
-def evaluate(user, pc ):
-    print(f"PC plays {keys[pc]}")
-    if win[user] == pc:
-        print("you win")
-    elif win[pc] == user:
-        print("you lose")
-    else:
-        print("it is a draw")
-
-def continuation():
-    play_desire = input("Do you want to plyay again? y/n \n")
-    if play_desire == "y":
-        return 1
-    else:
-        return 0
-
-
-status = 1
-while (status) :
+def play():
     user_play = input("Rock(r), paper(p) or scissors(s) ? \n ").lower()
     pc_play = random.choice(playlist)
-    evaluate(user_play, pc_play)
-    status = continuation()
+    print(f"PC plays {names[pc_play]}")
+
+    if is_win(user_play,pc_play):
+        return("You win")
+    elif user_play == pc_play:
+        return('It is a draw')
+    else:
+        return "You lose"
+
+
+def is_win(player, opponent):
+    if keys[player] == opponent:
+        return True
+    
+def continuation():
+    play_desire = input("Do you want to play again? y/n \n")
+    if play_desire == "y":
+        return True
+    else:
+        return False
+
+online = True
+while online:
+    print(play())
+    online = continuation() 
